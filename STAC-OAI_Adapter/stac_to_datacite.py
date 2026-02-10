f3cf04637fed3552a2c36c090587dd260adc8ccb
oussemajerfel12 <oussemajerfel@gmail.com>
2026-02-10 15:04:06 +0100

    Code Review

--- STAC-OAI_Adapter/stac_to_datacite.py
+++ STAC-OAI_Adapter/stac_to_datacite.py
@@ -1,226 +1,266 @@
+import threading
+if hasattr(threading, "_DummyThread"):
+    threading._DummyThread._delete = lambda self: None
+
 import os
 import sys
+import re
 import json
 from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from utils.stac_api import StacApiUtils
-from utils.datacite_utils import DataciteExportXML, prettify_xml
+from utils.datacite_utils import DataciteExportXML
 
 EXPORT_JSON_DIR = "./exports"
 EXPORT_XML_DIR = os.path.join(os.path.expanduser("~"), "Downloads", "oai_aire_records")
 MAX_WORKERS = 8
 
 
-def stac_to_datacite(collection, items=None):
-    creation_date = collection.get("creation_date")
-    pub_year = None
-    if creation_date:
-        try:
-            pub_year = datetime.fromisoformat(creation_date.replace("Z", "")).year
-        except Exception:
-            pass
+def safe_year(date_str):
+    if not date_str:
+        return None
+    try:
+        return datetime.fromisoformat(date_str.replace("Z", "")).year
+    except Exception:
+        return None
+
+
+def safe_filename(value: str, max_len=150) -> str:
+    value = value.strip()
+    value = re.sub(r'[<>:"/\\|?*]', '_', value)
+    value = re.sub(r'\s+', '_', value)
+    return value[:max_len]
+
+
+def extract_id(data: dict, is_item=False) -> str:
+    if is_item:
+        if "id" in data:
+            return data["id"]
+        ident = data.get("identifier", {}).get("identifier", "")
+        return ident.split("/")[-1] if ident else "record"
+
+    if "id" in data:
+        return data["id"]
+    return "record"
+
+
+
+def stac_collection_to_datacite(collection, base_url, items=None):
+    pub_year = safe_year(collection.get("creation_date"))
+
+    creators = (
+        [{"creatorName": p.get("name", "Unknown")} for p in collection.get("providers", [])]
+        or [{"creatorName": collection.get("cmip6:institution_id", "Unknown")}]
+    )
 
     related = []
     if items:
         for item in items:
             related.append({
-                "relatedIdentifier": f"{collection['id']}/items/{item['id']}",
+                "relatedIdentifier": f"{base_url}/collections/{collection['id']}/items/{item['id']}",
                 "relatedIdentifierType": "URL",
                 "relationType": "HasPart"
             })
 
-    creators = [{"creatorName": p.get("name", "Unknown")} for p in collection.get("providers", [])] \
-        or [{"creatorName": collection.get("cmip6:institution", collection.get("cmip6:institution_id", "Unknown"))}]
-
     return {
-        "identifier": {"identifier": f"{collection.get('id')}", "identifierType": "URL"},
+        "identifier": {
+            "identifier": f"{base_url}/collections/{collection['id']}",
+            "identifierType": "URL"
+        },
         "creators": creators,
-        "titles": [{"title": collection.get("title", "No Title")}],
+        "titles": [{"title": collection.get("title", collection["id"])}],
         "publisher": collection.get("institution_id", "CMCC"),
         "publicationYear": pub_year,
-        "resourceType": {"resourceTypeGeneral": "Collection", "resourceType": "Data Collection"},
-        "descriptions": [{"description": collection.get("description", ""), "descriptionType": "Abstract"}],
+        "resourceType": {
+            "resourceTypeGeneral": "Collection",
+            "resourceType": "Data Collection"
+        },
+        "descriptions": [{
+            "description": collection.get("description", ""),
+            "descriptionType": "Abstract"
+        }],
         "subjects": [{"subject": k} for k in collection.get("keywords", [])],
         "relatedIdentifiers": related
     }
 
-def stac_item_to_datacite(item):
+
+def stac_item_to_datacite(item, base_url):
     props = item.get("properties", {})
     assets = item.get("assets", {})
     links = item.get("links", [])
 
-    col_id = item.get('collection')
-    item_id = item.get('id')
-    identifier_val = f"https://catalogue.eneslab.pilot.eosc-beyond.eu/collections/{col_id}/items/{item_id}"
-    identifier_type = "URL"
-
-    creation_date = props.get("creation_date") or props.get("datetime")
-    pub_year = None
-    if creation_date:
-        try:
-            pub_year = datetime.fromisoformat(creation_date.replace("Z", "")).year
-        except Exception:
-            pass
+    col_id = item["collection"]
+    item_id = item["id"]
+    pub_year = safe_year(props.get("creation_date") or props.get("datetime"))
 
     creators = []
     if props.get("contact"):
         creators.append({
             "creatorName": props["contact"],
-            "affiliation": props.get("cmip6:institution_id", "None")
+            "affiliation": props.get("cmip6:institution_id")
         })
 
-    titles = [{"title": item.get("id")}]
-    publisher = props.get("cmip6:institution_id", "None")
-    license = "Creative Commons Attribution-ShareAlike"
-
-    subjects = [{"subject": props[k]} for k in [
-        "cmip6:mip_era","cmip6:activity_id","cmip6:institution_id","cmip6:source_id",
-        "cmip6:experiment_id", "cmip6:variant_label", "cmip6:table_id", "cmip6:variable_id", "cmip6:realm"
-    ] if props.get(k)]
-
-    dates = [{"date": creation_date, "dateType": "Issued"}] if creation_date else []
-    language = "eng"
-
-    resource_type = {
-        "resourceTypeGeneral": "dataset",
-        "uri": "http://purl.org/coar/resource_type/c_ddb1",
-        "resourceType": "dataset"
-    }
-
-    alternate_identifiers = [{"alternateIdentifier": item.get("id"), "alternateIdentifierType": "STAC-ID"}]
-    contributor = "CMCC"
+    subjects = [
+        {"subject": props[k]}
+        for k in [
+            "cmip6:mip_era", "cmip6:activity_id", "cmip6:institution_id",
+            "cmip6:source_id", "cmip6:experiment_id", "cmip6:variant_label",
+            "cmip6:table_id", "cmip6:variable_id", "cmip6:realm"
+        ]
+        if props.get(k)
+    ]
 
     related = []
     for asset in assets.values():
         if asset.get("href"):
-            related.append({"relatedIdentifier": asset["href"], "relatedIdentifierType": "URL", "relationType": "HasPart"})
-    if props.get("cmip6:further_info_url"):
-        related.append({"relatedIdentifier": props["cmip6:further_info_url"], "relatedIdentifierType": "URL", "relationType": "IsDescribedBy"})
-    for l in links:
-        if l.get("href"):
-            rel = l.get("rel", "related")
-            relationType = "IsMetadataFor" if rel=="self" else "IsPartOf" if rel=="collection" else "Related"
-            related.append({"relatedIdentifier": l["href"], "relatedIdentifierType": "URL", "relationType": relationType})
-
-    if item.get("collection"):
-        related.append({"relatedIdentifier": item["collection"], "relatedIdentifierType": "Local", "relationType": "IsPartOf"})
-
-    formats = [a["type"] for a in assets.values() if a.get("type")]
-
-    version = {"uri":"http://purl.org/coar/version/c_970fb48d4fbd8a85","version":"VoR"}
-    rights_list = {"rightsURI":"http://purl.org/coar/access_right/c_abf2","rights":"open access"}
-    fundingReferences = [{
-        "funderName": "European Commission",
-        "funderIdentifier": "http://doi.org/10.13039/100018693",
-        "funderIdentifierType": "Crossref Funder ID",
-        "fundingStream": "HORIZON EUROPE Framework Programme",
-        "awardNumber": {"awardURI": "https://cordis.europa.eu/project/id/101131875","awardNumber": "101131875"},
-        "awardTitle": "EOSC Beyond: advancing innovation and collaboration for research"
-    }]
-
-    descriptions = []
-    if props.get("cmip6:experiment"):
-        descriptions.append({"description": props.get("cmip6:experiment")})
-    if props.get("description"):
-        descriptions.append({"description": props.get("description")})
-
-    geo_locations = []
-    if "bbox" in item and len(item["bbox"])==4:
-        bbox = item["bbox"]
-        geo_locations.append({"geoLocationBox":{"westBoundLongitude":bbox[0],"southBoundLatitude":bbox[1],"eastBoundLongitude":bbox[2],"northBoundLatitude":bbox[3]}})
+            related.append({
+                "relatedIdentifier": asset["href"],
+                "relatedIdentifierType": "URL",
+                "relationType": "HasPart"
+            })
+    for link in links:
+        if link.get("href"):
+            rel = link.get("rel")
+            relation_type = (
+                "IsMetadataFor" if rel == "self"
+                else "IsPartOf" if rel == "collection"
+                else "Related"
+            )
+            related.append({
+                "relatedIdentifier": link["href"],
+                "relatedIdentifierType": "URL",
+                "relationType": relation_type
+            })
+    related.append({
+        "relatedIdentifier": f"{base_url}/collections/{col_id}",
+        "relatedIdentifierType": "URL",
+        "relationType": "IsPartOf"
+    })
 
     return {
-        "identifier": {"identifier": identifier_val, "identifierType": identifier_type},
+        "identifier": {
+            "identifier": f"{base_url}/collections/{col_id}/items/{item_id}",
+            "identifierType": "URL"
+        },
         "creators": creators,
-        "titles": titles,
-        "publisher": publisher,
-        "publicationYear": str(pub_year) if pub_year else None,
+        "titles": [{"title": item_id}],
+        "publisher": props.get("cmip6:institution_id", "CMCC"),
+        "publicationYear": pub_year,
         "subjects": subjects,
-        "dates": dates,
-        "language": language,
-        "resourceType": resource_type,
-        "alternateIdentifiers": alternate_identifiers,
+        "language": "eng",
+        "resourceType": {
+            "resourceTypeGeneral": "Dataset",
+            "resourceType": "Dataset",
+            "uri": "http://purl.org/coar/resource_type/c_ddb1"
+        },
+        "alternateIdentifiers": [{
+            "alternateIdentifier": item_id,
+            "alternateIdentifierType": "STAC-ID"
+        }],
         "relatedIdentifiers": related,
-        "formats": formats,
-        "version": version,
-        "rightsList": rights_list,
-        "descriptions": descriptions,
-        "geoLocations": geo_locations,
-        "fundingReferences": fundingReferences,
-        "contributor": contributor,
-        "license": license
+        "formats": [a["type"] for a in assets.values() if a.get("type")],
+        "rightsList": {
+            "rights": "open access",
+            "rightsURI": "http://purl.org/coar/access_right/c_abf2"
+        },
+        "descriptions": [{"description": props.get("description", "")}],
+        "fundingReferences": [{
+            "funderName": "European Commission",
+            "funderIdentifier": "http://doi.org/10.13039/100018693",
+            "funderIdentifierType": "Crossref Funder ID",
+            "awardNumber": {
+                "awardURI": "https://cordis.europa.eu/project/id/101131875",
+                "awardNumber": "101131875"
+            },
+            "awardTitle": "EOSC Beyond"
+        }]
     }
 
-def extract_id(data: dict) -> str:
-    if data.get("id"):
-        return data["id"]
-    titles = data.get("titles")
-    if isinstance(titles, str):
-        return titles.replace(".", "_")
-    if isinstance(titles, list) and titles:
-        title = titles[0].get("title")
-        if title:
-            return title.replace(".", "_")
-    return ""
 
-def export_datacite(enteries, mapper, label, subdir=None):
-    target_dir = os.path.join(EXPORT_JSON_DIR, label)
+def export_json(records, label, subdir=None, is_item=False):
+    target = os.path.join(EXPORT_JSON_DIR, label)
     if subdir:
-        target_dir = os.path.join(target_dir, subdir)
-    os.makedirs(target_dir, exist_ok=True)
-    for entry in enteries:
-        data = entry[1] if isinstance(entry, tuple) else entry
-        datacite_json = mapper(data)
-        id_ = extract_id(data)
-        if id_:
-            output = os.path.join(target_dir, f"{id_}.json")
-            with open(output, "w") as f:
-                json.dump(datacite_json, f, indent=2)
+        target = os.path.join(target, subdir)
+    os.makedirs(target, exist_ok=True)
+
+    for rec in records:
+        rec_id = extract_id(rec, is_item=is_item)
+        path = os.path.join(target, f"{rec_id}.json")
+        with open(path, "w", encoding="utf-8") as f:
+            json.dump(rec, f, indent=2)
 
 
-def fetch_items_for_collection(api, col):
-    items = api.get_items(col["id"])
-    print(f"{col['id']}: fetched {len(items)} items")
-    return col, items
+def fetch_items(api, collection):
+    items = api.get_items(collection["id"])
+    print(f"{collection['id']}: {len(items)} items")
+    return collection, items
+
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python stac_to_datacite_parallel.py <STAC catalog URL>")
+        print("Usage: python stac_to_datacite.py <STAC URL>")
         sys.exit(1)
 
-    catalog_url = sys.argv[1]
-    print("Fetching catalog from:", catalog_url)
-    api = StacApiUtils(catalog_url)
+    input_url = sys.argv[1]
+    print("Fetching:", input_url)
+
+    api = StacApiUtils(input_url)
     collections = api.get_collections()
-    if not collections:
-        print("No collections found!")
-        sys.exit(0)
-    print(f"Found {len(collections)} collections")
+
+    if "/collections/" in input_url:
+        match = re.match(r"(.*)/collections/?([^/]*)$", input_url)
+        if not match:
+            print("Invalid collection URL")
+            sys.exit(1)
+
+        base_url, collection_id = match.groups()
+
+        api = StacApiUtils(base_url)
+        collections = api.get_collections()
+
+        if collection_id:
+            collections = [c for c in collections if c["id"] == collection_id]
+
+            if not collections:
+                print(f"Collection {collection_id} not found")
+                sys.exit(1)
+
 
     all_items = {}
+    if len(collections) > 1:
+        with ThreadPoolExecutor(max_workers=6) as executor:
+            futures = [executor.submit(fetch_items, api, col) for col in collections]
+            for future in as_completed(futures):
+                try:
+                    col, items = future.result()
+                    all_items[col["id"]] = items
+                except Exception as e:
+                    print(f"Error fetching items for collection {col['id']}: {e}")
+    
 
-    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        futures = {executor.submit(fetch_items_for_collection, api, col): col for col in collections}
-        for future in as_completed(futures):
-            col, items = future.result()
-            all_items[col["id"]] = items
+    else:
+        col = collections[0]
+        all_items[col["id"]] = api.get_items(col["id"])
 
     for col in collections:
-        col_id = col["id"]
-        items = all_items.get(col_id, [])
-        print(f"Processing collection: {col_id} with {len(items)} items")
+        items = all_items[col["id"]]
+        if not items:
+            print(f"Collection {col['id']} has no items, skipping item export")
+            continue
 
-        if items:
-            datacite_items = [stac_item_to_datacite(item) for item in items]
-            export_datacite(datacite_items, lambda x: x, "item", subdir=col_id)
-            DataciteExportXML.export_oai_aire(datacite_items, EXPORT_XML_DIR)
+        item_dc = [stac_item_to_datacite(i, api.base_url) for i in items]
+        col_dc = stac_collection_to_datacite(col, api.base_url, items)
 
-        col_json = stac_to_datacite(col, items=items)
-        export_datacite([col_json], lambda x: x, "collection")
-        DataciteExportXML.export_oai_aire([col_json], EXPORT_XML_DIR)
+        if item_dc:
+            export_json(item_dc, "items", col["id"], is_item=True)
+            DataciteExportXML.export_oai_aire(item_dc, EXPORT_XML_DIR)
 
-    print("Datacite export complete!")
+        export_json([col_dc], "collections")
+        DataciteExportXML.export_oai_aire([col_dc], EXPORT_XML_DIR)
+
+    print("Datacite export complete")
+
 
 if __name__ == "__main__":
     main()
--- STAC-OAI_Adapter/utils/stac_api.py
+++ STAC-OAI_Adapter/utils/stac_api.py
@@ -3,14 +3,21 @@ from urllib.parse import urljoin
 
 class StacApiUtils:
     def __init__(self, base_url):
+        if "/collections/" in base_url:
+            base_url = base_url.split("/collections/")[0]
         self.base_url = base_url.rstrip("/")
 
     def get_collections(self):
         r = requests.get(f"{self.base_url}/collections")
         r.raise_for_status()
         return r.json().get("collections", [])
+    
+    def get_collection(self, collection_id):
+        r = requests.get(f"{self.base_url}/collections/{collection_id}")
+        r.raise_for_status()
+        return r.json()
 
-    def get_items(self, collection_id):
+    def get_items(self, collection_id, limit=None):
         items = []
         url = f"{self.base_url}/collections/{collection_id}/items"
 
@@ -20,8 +27,9 @@ class StacApiUtils:
             data = r.json()
 
             for item in data.get("features", []):
-                item["collection"] = collection_id
                 items.append(item)
+                if limit and len(items) >= limit:
+                    return items
 
             url = next(
                 (l["href"] for l in data.get("links", []) if l.get("rel") == "next"),
@@ -29,3 +37,4 @@ class StacApiUtils:
             )
 
         return items
+
